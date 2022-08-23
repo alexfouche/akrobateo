96dbe64427dfba537d24e41631bd7293fe565bb8
Alexandre Fouche <alexandre.fouche@gmail.com>
2022-08-23 10:56:32 +0200

    Update akrobateo-lb with latest updates from https://github.com/k3s-io/klipper-lb/blob/master/package

--- lb-image/entrypoint.sh
+++ lb-image/entrypoint.sh
@@ -1,20 +1,31 @@
 #!/bin/sh
+
+# forked from https://github.com/kontena/akrobateo/tree/master/lb-image
+# and updated from https://github.com/k3s-io/klipper-lb/blob/master/entry
+
 set -e -u
 
 trap exit TERM INT
 
-# Validate that we can operate
-if [ `cat /proc/sys/net/ipv4/ip_forward` != 1 ]; then
-    echo "ip_forward is not enabled"
-    exit 1
-fi
-
 echo "Setting up forwarding from port $SRC_PORT to $DEST_IP:$DEST_PORT/$DEST_PROTO"
 
 # Setup the actual forwarding
-sudo iptables -t nat -I PREROUTING ! -s ${DEST_IP}/32 -p ${DEST_PROTO} --dport ${SRC_PORT} -j DNAT --to ${DEST_IP}:${DEST_PORT}
-sudo iptables -t nat -I POSTROUTING -d ${DEST_IP}/32 -p ${DEST_PROTO} -j MASQUERADE
-
+if echo ${DEST_IP} | grep -Eq ":"
+then
+    if [ `cat /proc/sys/net/ipv6/conf/all/forwarding` != 1 ]; then
+        echo "forwarding is not enabled"
+        exit 1
+    fi
+    ip6tables -t nat -I PREROUTING ! -s ${DEST_IP}/128 -p ${DEST_PROTO} --dport ${SRC_PORT} -j DNAT --to [${DEST_IP}]:${DEST_PORT}
+    ip6tables -t nat -I POSTROUTING -d ${DEST_IP}/128 -p ${DEST_PROTO} -j MASQUERADE
+else
+    if [ `cat /proc/sys/net/ipv4/ip_forward` != 1 ]; then
+        echo "ip_forward is not enabled"
+        exit 1
+    fi
+    iptables -t nat -I PREROUTING ! -s ${DEST_IP}/32 -p ${DEST_PROTO} --dport ${SRC_PORT} -j DNAT --to ${DEST_IP}:${DEST_PORT}
+    iptables -t nat -I POSTROUTING -d ${DEST_IP}/32 -p ${DEST_PROTO} -j MASQUERADE
+fi
 
 echo "Forwarding set up succesfully, taking Cinderella nap..."
 
