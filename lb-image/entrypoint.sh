c2fce421045c3efc3e259c4bb21aa224a77dae37
Alexandre Fouche <alexandre.fouche@gmail.com>
2022-09-07 17:32:17 +0200

    Do not exit if Sysctl IP forwarding not set

--- lb-image/entrypoint.sh
+++ lb-image/entrypoint.sh
@@ -14,14 +14,14 @@ if echo ${DEST_IP} | grep -Eq ":"
 then
     if [ `cat /proc/sys/net/ipv6/conf/all/forwarding` != 1 ]; then
         echo "forwarding is not enabled"
-        exit 1
+        # exit 1
     fi
     ip6tables -t nat -I PREROUTING ! -s ${DEST_IP}/128 -p ${DEST_PROTO} --dport ${SRC_PORT} -j DNAT --to [${DEST_IP}]:${DEST_PORT}
     ip6tables -t nat -I POSTROUTING -d ${DEST_IP}/128 -p ${DEST_PROTO} -j MASQUERADE
 else
     if [ `cat /proc/sys/net/ipv4/ip_forward` != 1 ]; then
         echo "ip_forward is not enabled"
-        exit 1
+        # exit 1
     fi
     iptables -t nat -I PREROUTING ! -s ${DEST_IP}/32 -p ${DEST_PROTO} --dport ${SRC_PORT} -j DNAT --to ${DEST_IP}:${DEST_PORT}
     iptables -t nat -I POSTROUTING -d ${DEST_IP}/32 -p ${DEST_PROTO} -j MASQUERADE
